7b06ac38623f951960f518284ec3f060e1785d3c
adityamaurya-git <145964251+adityamaurya-git@users.noreply.github.com>
2025-10-24 16:54:02 +0530

    file path updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 const corsOptions = {
@@ -34,6 +34,6 @@ app.use('/api/messages' , messageRoutes);
 
 
 app.get('*name' , (req ,res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,  '../public/index.html'));
 })
 module.exports = app;
